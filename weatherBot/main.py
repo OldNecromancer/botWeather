fad895fff9b7707618a60e46a6df9fdd12bf5a6d
OldNecromancer <dushak01@mail.ru>
2024-11-13 16:48:05 +0300

    Update main.py
    - deleted verify false

--- weatherBot/main.py
+++ weatherBot/main.py
@@ -39,7 +39,7 @@ def callback_handler(call: CallbackQuery):
 @bot.message_handler(content_types=['text'])
 def get_weather(message):
     city = message.text.strip().lower()
-    result = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("open_weather_token")}&units=metric', verify=False)
+    result = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("open_weather_token")}&units=metric')#, verify=False)
     if result.status_code == 200:
         data = json.loads(result.text)
         bot.reply_to(message, f'Погода сейчас: {data["main"]["temp"]}°C')
